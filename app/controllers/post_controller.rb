d55e888babe40cd26c5509c6cb2e69fd8541d2e5
yamakei <57038453+yamakei135@users.noreply.github.com>
2019-11-23 22:33:52 +0900

    modify post function

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,14 +3,14 @@ class PostController < ApplicationController
   end
 
   def create
-      @post = Post.new(posted_by: "test",
+      @post = Post.new(posted_by: current_user.name,
                         restaurant_name: params[:restaurant_name],
                         restaurant_adress: params[:restaurant_adress],
                         restaurant_adress_url: params[:restaurant_adress_url],
                         cost: params[:cost],
                         rating: params[:rating],
                         comment: params[:comment])
-                        
+
       if @post.save
         redirect_to action: :index
         flash[:notice] = "投稿されました🐼"
@@ -24,7 +24,6 @@ class PostController < ApplicationController
   end
 
   def index
-    @user = User.find_by(name: params[:name])
-    @posts = Post.find_by(posted_by: params[:name])
+    @posts = Post.where(posted_by: current_user.name)
   end
 end
