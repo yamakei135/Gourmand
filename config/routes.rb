6646fc6a77b073bae005ad2363f0ff65c978b85d
片寄諒亮 <lyosuke@lksMacBook-Pro.local>
2019-12-11 19:02:22 +0900

    profile-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   get '/'=>'home#top'
   get 'home/about'=>'home#about'
-  get 'user/login'=>'user#login'
+  get 'session/login'=>'session#form'
+  post 'session/login'=>'session#login'
   get 'user/registration'=>'user#registration'
+  post 'user/create'=>'user#create'
   get 'mypage/profile'=>'mypage#profile'
+  post 'post/create' => 'post#create'
   get 'mypage/edit'=>'mypage#edit'
   get 'post/new'=>'post#new'
   get 'post/detail'=>'post#detail'
+  get 'post/index' => 'post#index'
+  delete 'session/logout'=>'session#logout'
+  get 'post/:id/edit'=>'post#edit'
+  post 'post/:id/update'=>'post#update'
+  post 'post/:id/destroy'=>'post#destroy'
+  post 'user/:id/update'=>'user#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
