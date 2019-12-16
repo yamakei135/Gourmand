791bc79adf93379c60295abd5add211567fd4e21
yamakei <57038453+yamakei135@users.noreply.github.com>
2019-12-16 15:15:18 +0900

    add profile image func

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,8 @@
 class MypageController < ApplicationController
 
   def profile
+    @post = Post.find_by(posted_by: current_user.id)
+    @posts = Post.where(posted_by: current_user.id)
   end
 
   def edit
