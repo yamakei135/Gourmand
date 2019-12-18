8f6a0e11fb5dcfb21dbf71b0705b419a811652ff
yamakei <57038453+yamakei135@users.noreply.github.com>
2019-12-18 22:42:14 +0900

    for harako

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -3,9 +3,11 @@ class MypageController < ApplicationController
   def profile
     @post = Post.find_by(posted_by: current_user.id)
     @posts = Post.where(posted_by: current_user.id)
+    @user = User.find_by(id: current_user.id)
   end
 
   def edit
+    @user = User.find_by(id: current_user.id)
   end
 
 end
