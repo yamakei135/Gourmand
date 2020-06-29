7619e82b9cca0254737db4e986c84023eba887ae
yamakei <57038453+yamakei135@users.noreply.github.com>
2020-06-29 19:27:53 +0900

    レスポンシブ調整

--- db/migrate/router.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Users from './views/Users.vue';
-Vue.use(Router); //プラグインを適用
-
-export default new Router({
-  routes: [{path: '/',component: Home},{path: '/users', component: Users}]
-});
